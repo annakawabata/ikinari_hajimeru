b8bd89f0d167c690debb672c1d30b8e98f96e301
川端杏奈 <Anna2301a89>
2015-10-01 23:10:25 +0800

    文字化け/１時間

---
+++ check.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="UTF-8">
+  
+  <title>チェックPHP</title>
+
+  
+  </head>
+  <body>
+    <?php
+    print'ようこそ';
+    print $_POST['nickname'];
+    print '様';
+    ?>
+
+  </body>
+</html>
