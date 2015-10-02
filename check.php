230ad5ac0cdad5c872c7bbe295286129155e4455
川端杏奈 <Anna2301a89>
2015-10-02 20:28:47 +0800

    フォーム追加

--- check.php
+++ check.php
@@ -2,16 +2,43 @@
 <html>
   <head>
   <meta charset="UTF-8">
-  
+
   <title>チェックPHP</title>
 
   
   </head>
   <body>
     <?php
-    print'ようこそ';
-    print $_POST['nickname'];
-    print '様';
+    $nickname = $_POST['nickname'];
+    $email = $_POST['email'];
+    $goiken = $_POST['goiken'];
+
+    if($nickname == ''){
+    	print'ニックネームが入力されていません。';
+    }else{
+    	print'ようこそ';
+    	print $nickname;
+    	print '様';
+    	print'</br>';
+    }
+
+    if($email == ''){
+    	print'メールアドレスが入力されていません。';
+    }else{
+    	print'メールアドレス';
+    	print $email;
+    	print'</br>';
+    }
+
+    if($goiken == ''){
+    	print'ご意見が入力されていません。';
+    }else{
+    	print'ご意見';
+    	print $goiken;
+    	print'</br>';
+    }
+
+
     ?>
 
   </body>
