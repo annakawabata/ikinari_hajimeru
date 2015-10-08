ecd9293e4d9b66a7dae817cf0695d00567e7144d
川端杏奈 <Anna2301a89>
2015-10-08 11:49:25 +0800

    編集機能：途中

--- update.php
+++ update.php
@@ -1,17 +1,33 @@
 <?php
-
+    //編集時に必要な情報
 	session_start();
 	$db = mysqli_connect('localhost', 'root', 'root', 'ikinari_hajimeru') or die(mysqli_connect_error());
 	        mysqli_set_charset($db, 'utf8');
 
+	//編集時に必要な情報
+	  //$tableの中に anketo　の情報を全部入れている
 	if(isset($_REQUEST['code'])){
 	$sql = sprintf('SELECT * FROM anketo WHERE code=%d',
 		mysqli_real_escape_string($db,$_REQUEST['code']));
 	$record = mysqli_query($db,$sql) or die(mysqli_error($db));
 	$table = mysqli_fetch_array($record);
 }
-?>
 
+//更新している
+if(!empty($_POST)){
+	if($_POST['goiken'] != ''){
+		$sql = sprintf('UPDATE * SET goiken="%s" WHERE id=%d',
+			mysqli_real_escape_string($db,$_POST['goiken']),
+			mysqli_real_escape_string($db,$_GET['code']));
+
+			mysqli_query($db,$sql) or die(mysqli_error($db));
+
+			//これすることによって再読込ボタンを押したことによる、二重投稿を防止している。
+			header('Location:menu.html');
+			exit();
+	}
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -21,7 +37,8 @@
 
   </head>
   <body>
-
+  <!--<?php if(isset($_REQUEST['code'])){print $table['nickname'].'様';}?>
+  これはcode(id)をリクエストしてその中のデータをプリントしている-->
   ニックネーム:<?php if(isset($_REQUEST['code'])){print $table['nickname'].'様';}?></br>
   メールアドレス:<?php if(isset($_REQUEST['code'])){print $table['email'];}?></br>
   ご意見を編集してください:</br>
